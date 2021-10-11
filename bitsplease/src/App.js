bb3afacba1ab26bc7e1aca48fd346c6af485aa45
Jd Perumal <u18219285@tuks.co.za>
2021-10-11 23:41:31 +0200

    Implemented basic routing through home page buttons

--- bitsplease/src/App.js
+++ bitsplease/src/App.js
@@ -1,21 +1,51 @@
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <ul>
-          <li ><a className="App-link" href="#">Projects</a></li>
-          <li ><a className="App-link" href="#">Shop</a></li>
-          <li ><a className="App-link" href="#">About us</a></li>
-          <li ><a className="App-link" href="#">Our team</a></li>
-          <li ><a className="App-link" href="#">Contact us</a></li>
-        </ul>
-      </header>
+    <Router>
 
-    </div>
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Bits Please</h1>
+          <ul>
+            <li ><Link className="App-link" to="/">Home</Link></li>
+            <li > <Link className="App-link" to="/about">Shop</Link></li>
+            <li > <Link className="App-link" to="/about">About Us</Link></li>
+            <li > <Link className="App-link" to="/about">Our team</Link></li>
+            <li > <Link className="App-link" to="/about">Contact us</Link></li>
+          </ul>
+        </header>
+
+      </div>
+      {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+      <Switch>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
+}
+
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About us</h2>;
 }
 
 export default App;
