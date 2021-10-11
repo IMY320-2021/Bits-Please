01f02df75a2fdc8942465b518e56879652ca7c05
Jd Perumal <u18219285@tuks.co.za>
2021-10-11 15:39:17 +0200

    Updated new folder structure

--- bitsplease/src/App.js
+++ bitsplease/src/App.js
@@ -9,8 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>
-          Jd was here    </p>
+        <p>Jd was here</p>
         <a
           className="App-link"
           href="https://reactjs.org"
