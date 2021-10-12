4a582f7b923d4266094cc2e0018795725057b5bb
ChanelMulder <chanel.mulder@gmail.com>
2021-10-12 16:05:31 +0200

    ProjectsPage setup
    I added placeholder images, for the cars and created the general page layout for this page, I still need to implement the scrolling animation before it is finished 100%

---
+++ bitsplease/src/pages/ProjectsPage.js
@@ -0,0 +1,37 @@
+function ProjectsPage() {
+  return (
+    <div className="App">
+      <header className="App-header">
+            <div className="project">
+                <div className="one">
+                    <button className="title"></button>
+                    <h1>Project 1</h1>
+                    <button className="image"><img className='animate-left' src={"./" + 'car1.jpg'}></img></button> 
+                </div>
+                <div className="two">
+                    <button className="title"></button>
+                    <button className="image"><img className='animate-right' src={"./" + 'car2.jpeg'}></img></button>
+                    <h1>Project 2</h1>
+                </div>
+                <div className="three">
+                    <button className="title"></button>
+                    <button className="image"><img className='animate-right' src={"./" + 'car3.jpg'}></img></button>
+                    <h1>Project 3</h1>
+                </div>
+                <div className="four">
+                    <button className="title"></button>
+                    <button className="image"><img className='animate-right' src={"./" + 'car4.jpg'}></img></button>
+                    <h1>Project 4</h1>
+                </div>
+                <div className="five">
+                    <button className="title"></button>
+                    <button className="image"><img src={"./" + 'car5.jpg'}></img></button>
+                    <h1>Project 5</h1>
+                </div>
+            </div>
+      </header>
+    </div>
+  );
+}
+
+export default ProjectsPage;
