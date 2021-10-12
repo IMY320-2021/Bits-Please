0d5d4af8a505d7b054bc3be86c7293a4ae8f5784
Jd Perumal <u18219285@tuks.co.za>
2021-10-12 14:22:46 +0200

    Create Projects.js
    Created Project page file

---
+++ bitsplease/src/pages/Projects.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from "react";
+import '../App.css';
+
+export default function Projects() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>Temp projects page</p>
+            </header>
+        </div>
+    );
+}
+
