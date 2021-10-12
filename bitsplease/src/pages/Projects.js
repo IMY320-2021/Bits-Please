10d0d38acce61eafb8e9f5055759edf5c8cea869
ChanelMulder <chanel.mulder@gmail.com>
2021-10-12 21:12:47 +0200

    Scrolling implementation
    When scrolling down the content moves sideways. Arrow animation for showing user to scroll down is also added. Black arrow added at the top for a "Home" button. (functionality to go home is not implemented).

--- bitsplease/src/pages/Projects.js
+++ bitsplease/src/pages/Projects.js
@@ -1,13 +1,45 @@
 import React from "react";
-import '../App.css';
+import './Projects.css';
 
-export default function Projects() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <p>Temp projects page</p>
-            </header>
-        </div>
-    );
+function Projects() {
+    
+  return (
+    <div className="App">
+      <header className="App-header">
+      
+          <div className="outerWrapper">
+            <div className="wrapper">
+                <div className="project1">
+                    <button className="title"></button>
+                    <h1 className="titleNotGlow">Project 1</h1>
+                    <button className="image"><img className='animate-left' src={"./" + 'car1.jpg'}></img></button> 
+                </div>
+                <div className="project2">
+                    <button className="image"><img className='animate-right' src={"./" + 'car2.jpeg'}></img></button>
+                    <h1 className="titleNotGlow">Project 2</h1>
+                </div>
+                <div className="project3">
+                    <button className="image"><img className='animate-right' src={"./" + 'car3.jpg'}></img></button>
+                    <h1 className="titleNotGlow">Project 3</h1>
+                </div>
+                <div className="project4">
+                    <button className="image"><img className='animate-right' src={"./" + 'car4.jpg'}></img></button>
+                    <h1 className="titleNotGlow">Project 4</h1>
+                </div>
+                <div className="project5">
+                    <button className="image"><img src={"./" + 'car5.jpg'}></img></button>
+                    <h1 className="titleNotGlow">Project 5</h1>
+                </div>
+                
+            </div>
+            </div>
+            <div className="arrowBox">
+                <div className="arrow"></div>
+            </div>
+            <img className="Home" src={"./" + 'arrow_back_black_24dp.svg'}></img>
+      </header>
+    </div>
+  );
 }
 
+export default Projects;
--- bitsplease/src/pages/ProjectsPage.js
+++ bitsplease/src/pages/ProjectsPage.js
@@ -1,3 +1,6 @@
+import React from "react";
+import './Projects.css';
+
 function ProjectsPage() {
   return (
     <div className="App">
