9f21d66bdbb0b1f5a9165697d9065a3c69d4f381
Christoff <christoff.linde@gmail.com>
2021-10-12 20:44:15 +0200

    Temporarily remove some mock data

--- bitsplease/src/components/CardContainer/CardContainer.js
+++ bitsplease/src/components/CardContainer/CardContainer.js
@@ -21,22 +21,22 @@ const StoreData = {
             image_url: './image.png',
             options: ['XS', 'S ', 'M', 'L ', 'XL']
         },
-        {
-            id: 's-ts-01',
-            title: 'Short Sleeve T-Shirt',
-            description: '100% pure cotton short sleeve T-Shirt with printed graphic',
-            price: 129.95,
-            image_url: './image.png',
-            options: ['XS', 'S ', 'M', 'L ', 'XL']
-        },
-        {
-            id: 'l-ts-01',
-            title: 'Long Sleeve T-Shirt',
-            description: '100% pure cotton long sleeve T-Shirt with printed graphic',
-            price: 159.95,
-            image_url: './image.png',
-            options: ['XS', 'S ', 'M', 'L ', 'XL']
-        },
+        // {
+        //     id: 's-ts-01',
+        //     title: 'Short Sleeve T-Shirt',
+        //     description: '100% pure cotton short sleeve T-Shirt with printed graphic',
+        //     price: 129.95,
+        //     image_url: './image.png',
+        //     options: ['XS', 'S ', 'M', 'L ', 'XL']
+        // },
+        // {
+        //     id: 'l-ts-01',
+        //     title: 'Long Sleeve T-Shirt',
+        //     description: '100% pure cotton long sleeve T-Shirt with printed graphic',
+        //     price: 159.95,
+        //     image_url: './image.png',
+        //     options: ['XS', 'S ', 'M', 'L ', 'XL']
+        // },
     ]
 }
 
