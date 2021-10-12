232c41b98d3b4d36edde853765a8f377556e0060
Christoff <christoff.linde@gmail.com>
2021-10-12 20:39:56 +0200

    Enable dynamic content
    The component was updated to dynamically use the data passed in with props from the CardContainer.
    
    Additionally, unused mock .svg data was removed

--- bitsplease/src/components/StoreCard/StoreCard.js
+++ bitsplease/src/components/StoreCard/StoreCard.js
@@ -3,28 +3,24 @@ import './StoreCard.css';
 import '../../index.css';
 import CustomRadioGroup from '../CustomRadioGroup/CustomRadioGroup';
 
-const square = (
-    <svg width="36" height="36" viewBox="0 0 36 36" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <rect width="36" height="36" fill="#373737" />
-    </svg>
-);
-
 class StoreCard extends React.Component {
     render() {
+        const data = this.props.data;
+
         return (
             <div className='card'>
                 <div className='card-header'>
                     <div className='card-image'>
-                        <h1>IMG</h1>
+                        <h1>{ }</h1>
                     </div>
                 </div>
 
                 <div className='card-body'>
-                    <h2>TITLE</h2>
-                    <p>This is a description</p>
-                    <h2>R299.00</h2>
+                    <h2>{data.title}</h2>
+                    <p>{data.description}</p>
+                    <h2 className='price-heading'>R {data.price}</h2>
                     <h3>Color</h3>
-                    <CustomRadioGroup />
+                    <CustomRadioGroup data={data.options} />
                     <button className='btn btn-primary' type='button'>ADD TO CART</button>
                 </div>
             </div>
